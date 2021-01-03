52c565ed643762a079bf6a912b1ba4d5cc88a627
garcikg0 <kg.gar91@gmail.com>
2021-01-02 21:28:01 -0500

    Two Sum answer

---
+++ Two Sum II/index.js
@@ -0,0 +1,15 @@
+function twoSum(numbers, target) {
+    let left = 0
+    let right = numbers.length - 1
+    
+    while (left < right) {
+        const curSum = numbers[left] + numbers[right]
+        if (curSum < target) {
+            left++
+        } else if (curSum > target) {
+            right--
+        } else {
+            return [left +1, right+1]
+        }
+    }
+};
